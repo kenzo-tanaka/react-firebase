ffd3edc71f93eed37a020152c06fdb6de314e99d
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2019-05-31 18:08:08 +0900

    Adding Dummy Data

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
 import Notification from "./Notifications";
 import ProjectList from "../projects/ProjectList";
+import { connect } from "react-redux";
 
 class Dashboard extends Component {
   state = {};
   render() {
+    const { projects } = this.props; // @@ grabs the projects object
+
     return (
       <div className="dashboard container">
         <div className="row">
           <div className="col s12 m6">
-            <ProjectList />
+            <ProjectList projects={projects} />{" "}
+            {/* @@ pass projects to ProjectList */}
           </div>
           <div className="col s12 m5 offset-m1">
             <Notification />
@@ -20,4 +24,11 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard;
+// @@ map our state from store to the props
+const mapStateToProps = state => {
+  return {
+    projects: state.project.projects
+  };
+};
+
+export default connect(mapStateToProps)(Dashboard);
--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ProjectSummary from "./ProjectSummary";
 
-const ProjectList = () => {
+const ProjectList = ({ projects }) => {
   return (
     <div className="project-list section">
-      <ProjectSummary />
-      <ProjectSummary />
-      <ProjectSummary />
+      {/* @@ if we have projects, do below */}
+      {projects &&
+        projects.map(project => {
+          return <ProjectSummary project={project} key={project.id} />; // @@ pass individual project
+        })}
     </div>
   );
 };
