46b131be51e4006cb85185d43e7b1de034a11f5e
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2019-05-31 16:37:25 +0900

    Login & Signup Components

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 
-class SignIn extends Component {
+class SignUp extends Component {
   state = {
     email: "",
-    password: ""
+    password: "",
+    firstName: "",
+    lastName: ""
   };
 
   handleChange = e => {
@@ -20,7 +22,7 @@ class SignIn extends Component {
     return (
       <div className="container">
         <form onSubmit={this.handleSubmit} className="white">
-          <h5 className="grey-text text-darken-3">Sign In</h5>
+          <h5 className="grey-text text-darken-3">Sign Up</h5>
           <div className="input-field">
             <label htmlFor="email">Email</label>
             <input type="email" id="email" onChange={this.handleChange} />
@@ -30,7 +32,16 @@ class SignIn extends Component {
             <input type="password" id="password" onChange={this.handleChange} />
           </div>
           <div className="input-field">
-            <button className="btn pink lighten-1 z-depth-0">Login</button>
+            <label htmlFor="firstName">FirstName</label>
+            <input type="text" id="firstName" onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="lastName">LastName</label>
+            <input type="text" id="lastName" onChange={this.handleChange} />
+          </div>
+
+          <div className="input-field">
+            <button className="btn pink lighten-1 z-depth-0">SignUp</button>
           </div>
         </form>
       </div>
@@ -38,4 +49,4 @@ class SignIn extends Component {
   }
 }
 
-export default SignIn;
+export default SignUp;
