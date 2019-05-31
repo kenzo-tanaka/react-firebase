d2b77f4b7886876413ee5bf55607379e04b25909
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2019-05-31 14:53:30 +0900

    Dashboard Component

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import Notification from "./Notifications";
+import ProjectList from "../projects/ProjectList";
+
+class Dashboard extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="dashboard container">
+        <div className="row">
+          <div className="col s12 m6">
+            <ProjectList />
+          </div>
+          <div className="col s12 m5 offset-m1">
+            <Notification />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
