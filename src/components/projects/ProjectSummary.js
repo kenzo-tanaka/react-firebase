baefd506f0ba36271a42044d031090e95cc7ed29
kenzoukenzou <kenzou.kenzou104809@gmail.com>
2019-06-01 14:01:06 +0900

    Formatting dates with Moment #31

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,4 +1,5 @@
 import React from "react";
+import moment from "moment";
 
 const ProjectSummary = ({ project }) => {
   return (
@@ -8,7 +9,9 @@ const ProjectSummary = ({ project }) => {
         <p>
           Created by {project.authorFirstName} {project.authorLastName}
         </p>
-        <p className="grey-text">3rd September, 2am</p>
+        <p className="grey-text">
+          {moment(project.createdAt.toDate()).calendar()}
+        </p>
       </div>
     </div>
   );
